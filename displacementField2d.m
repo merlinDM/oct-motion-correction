4c032f13e9dd2f980e1960161d58398d056762eb
merlinDM <davydov.michael.a@gmail.com>
2019-05-31 23:04:09 +0300

    Add metric modeling.
    * Add script for metrics investigation. 
    * Fix bug in displacementField2d.
    * Fix relative position in saccades modeling.

--- displacementField2d.m
+++ displacementField2d.m
@@ -7,7 +7,8 @@ function [ D ] = displacementField2d(origin, m, n, fastAxis)
         n = m;
     end
 
-    cc = [origin.x; origin.y]';
+    ori = origin(1:(m * n));
+    cc = [ori.x; ori.y]';
     D = reshape(cc, m, n, 2);
 
     if strcmp(fastAxis, 'yfast')
